b9e4269e09e37612c1c0486e14988cb383475457
Serina Robinson <serina.robinson@wur.nl>
2020-03-13 15:43:51 -0500

    Upload AMP-binding.hmm

--- bin/figs/fig1c.r
+++ bin/figs/fig1c.r
@@ -26,22 +26,22 @@ for(i in 1:10) {
   dat_train <- training(dat_split)
   dat_test  <- testing(dat_split)
   nrow(dat_train)/nrow(dat) # 75 %
-  
+
+  # Define our response
   x_train <- dat_train[,!colnames(dat_train) %in% c("nms", "clf")]
   x_test <- dat_test[,!colnames(dat_test) %in% c("nms", "clf")]
   y_train <- as.factor(dat_train$clf)
   y_test <- as.factor(dat_test$clf)
+  print(table(y_test))
   
   # Complete dataset for training and testing
   form_train <- data.frame(cbind(x_train, y_train), stringsAsFactors = F, row.names = dat_train$nms)
   form_test <- data.frame(cbind(x_test, y_test), stringsAsFactors = F, row.names = dat_test$nms)
   
-  # Set class weights
   classwts <- nrow(dat)/(length(unique(dat$clf)) * table(dat$clf))
   dtf <- data.frame(cbind(table(dat$clf), classwts))
   colnames(dtf) <- c("class_size", "class_weights")
   
-  # Train random forest models
   rf_models[[i]] <- ranger(y_train ~., data = form_train, num.trees = 1000, splitrule = "gini",
                            mtry = as.integer(sqrt(ncol(x_train))), min.node.size = 1,
                            importance = "permutation", class.weights = classwts, probability = T)
@@ -50,31 +50,23 @@ for(i in 1:10) {
   dat_test[[i]] <- dat_test
 }
 
-# Best performing model is model 4
 # Calculate the overall test statistics
 ind <- 10
-rf_df <- data.frame(rf_pred[[ind]]$predictions)
-head(rf_df)
-
+rf_df <- rf_pred[[ind]]$predictions
 colnames(rf_df) <- paste0(colnames(rf_df), "_pred_RF")
 
 true_label <- dummies::dummy(dat_test$clf, sep = ".")
 true_label <- data.frame(true_label)
 colnames(true_label) <- paste0(colnames(true_label), "_true")
+true_label <- data.frame(true_label)
 
-
-final_df <- cbind(true_label, rf_df)
+final_df <- data.frame(cbind(true_label, rf_df), stringsAsFactors = F)
 colnames(final_df) <- gsub("clf\\.", "", colnames(final_df))
 
-roc_res <- multi_roc(final_df, force_diag = T)
-pr_res <- multi_pr(final_df, force_diag = T)
-
-rf_pred <- data.frame(rf_pred[[ind]]$predictions)
-colnames(rf_pred) <- paste(colnames(rf_pred), "_pred_RF")
-
-plot_roc_df <- plot_roc_data(roc_res) %>%
-  dplyr::filter(!Group %in% c("Micro", "Macro"))
-
+roc_res <- multi_roc(final_df, force_diag=T)
+warnings()
+pr_res <- multi_pr(final_df, force_diag=T)
+plot_roc_df <- plot_roc_data(roc_res)
 plot_pr_df <- plot_pr_data(pr_res)
 
 pal2 <- c("#E41A1C", "#92D050", "#377EB8", "#984EA3",
@@ -100,6 +92,7 @@ dev.off()
 # Calculate confidence intervals
 roc_ci_res_micro <- roc_ci(final_df, conf= 0.95, type='basic', R = 100, index = 17)
 roc_ci_res_micro
+roc_ci_res_micro$t0
 roc_ci_res_macro <- roc_ci(final_df, conf= 0.95, type='basic', R = 100, index = 1)
 roc_ci_res_macro
 roc_auc_with_ci_res <- roc_auc_with_ci(final_df, conf= 0.95, type='basic', R = 100)
